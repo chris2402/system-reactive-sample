ac26a3382d29c33d65e6e7ed5fee3fa3f541bac6
Christer Steinfinsbø <csteinfinsbo@gmail.com>
2024-10-17 12:26:41 +0200

    Hot Observable

--- Program.cs
+++ Program.cs
@@ -3,19 +3,34 @@
 // Uses current SynchronizationContext; SubscribeOn & ObserveOn
 
 var eventSource = new SomeEventSource();
-// Observable.FromEventPattern<SomeEventArg>(eventSource, nameof(SomeEventSource.OnEvent));
-Observable.FromEventPattern<EventHandler<SomeEventArg>, SomeEventArg>(
+
+_ = Task.Run(async () => 
+{
+    while(true)
+    {
+        eventSource.Trigger();
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
+});
+
+var hotObservable = Observable.FromEventPattern<EventHandler<SomeEventArg>, SomeEventArg>(
         h => eventSource.OnEvent += h,
         h => eventSource.OnEvent -= h)
-    .Select(x => x.EventArgs.When)
-    .Subscribe(when => Console.WriteLine(when));
+    .Select(x => x.EventArgs.When);
 
-// Execute
-eventSource.Trigger();
-await Task.Delay(TimeSpan.FromSeconds(1));
-eventSource.Trigger();
-await Task.Delay(TimeSpan.FromSeconds(2));
-eventSource.Trigger();
+await Task.Run(() => 
+{
+    bool shouldSubscribe = true;
+    while (shouldSubscribe)
+    {
+        Console.WriteLine("Starting subscription, hit enter to unsubscribe");
+        var subscription = hotObservable.Subscribe(when => Console.WriteLine(when));
+        Console.ReadLine();
+        subscription.Dispose();
+        Console.WriteLine("Subscription stopped, resubscribe? [Y]/n");
+        shouldSubscribe = (Console.ReadLine() ?? "").Trim().ToUpper() != "N";
+    }
+});
 
 public class SomeEventArg : EventArgs 
 {
