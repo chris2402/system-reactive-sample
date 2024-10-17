942e4abb7b4f34549c37c009a12a025b8ba2ae3a
Christer Steinfinsbø <csteinfinsbo@gmail.com>
2024-10-17 13:28:01 +0200

    Lame attempt on making a debounced string value

--- Program.cs
+++ Program.cs
@@ -1,21 +1,45 @@
 ﻿using System.Reactive.Linq;
+using System.Reactive.Subjects;
 
-Observable.Range(0, 100)
-    .Buffer(5)
-    .Subscribe(buffer => 
+// NB RUN IN CONSOLE > dotnet run 
+
+
+// Create a subject that will take string inputs
+var subject = new Subject<string>();
+int maxLengthSoFar = 0;
+
+// Set up an observable that emits on the first change,
+// then debounces for Xms with the latest value.
+var debouncedObservable = subject
+    .Throttle(TimeSpan.FromMilliseconds(250)) 
+    .DistinctUntilChanged(); 
+
+debouncedObservable.Subscribe(v =>
+{            
+    int currentLineCursor = Console.CursorTop;
+    Console.SetCursorPosition(0, currentLineCursor);
+    Console.Write(new string(' ', v.Length + 100));
+    Console.SetCursorPosition(0, currentLineCursor);
+    Console.Write(v);
+});
+
+Console.WriteLine("Start typing!");
+string inputBuffer = string.Empty;
+while (true)
+{
+    // Read a key without displaying it in the console
+    var keyInfo = Console.ReadKey(intercept: true);
+    if (keyInfo.Key == ConsoleKey.Backspace && inputBuffer.Length > 0)
     {
-        Console.WriteLine(string.Join(',', buffer));
-    });
+        inputBuffer = inputBuffer.Substring(0, inputBuffer.Length - 1);  // Remove last character
+        subject.OnNext(inputBuffer);  // Emit the updated string
+    }
 
-Console.ReadLine();
-
-// Will only sometimes reach 5 events within the time window 
-Observable.Interval(TimeSpan.FromSeconds(0.21)) 
-    .Window(TimeSpan.FromSeconds(1), 5)
-    .SelectMany(window => window.ToArray())
-    .Subscribe(array => 
+    // If the key is a valid character, add it to the input buffer and the subject
+    if (!char.IsControl(keyInfo.KeyChar))  // Only add printable characters
     {
-        Console.WriteLine(string.Join(',', array));
-    });
-
-Console.ReadLine();
+        inputBuffer += keyInfo.KeyChar;
+        maxLengthSoFar = Math.Max(inputBuffer.Length, maxLengthSoFar);
+        subject.OnNext(inputBuffer);  // Emit the current buffer
+    }
+}
