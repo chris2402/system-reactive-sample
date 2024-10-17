32ff1f728e55590ec678aa6bc0e9a5f35a1b6d3e
Christer Steinfinsbø <csteinfinsbo@gmail.com>
2024-10-17 11:54:43 +0200

    Enumerable conversion

--- Program.cs
+++ Program.cs
@@ -1,81 +1,29 @@
 ﻿using System.Reactive.Linq;
-using System.Reactive.Threading.Tasks;
 
-Console.WriteLine("Should we throw Y/[N]?");
-var shouldThrow = (Console.ReadLine() ?? "").Trim().ToUpper() == "Y"; 
+Enumerable.Range(1, 100)
+    .ToObservable()
+    .Subscribe(Console.WriteLine);
 
-async Task SomeDangerousOperation()
+
+foreach (var answer in Observable.Range(1, 100).ToEnumerable())
 {
-    if (shouldThrow) throw new Exception();
-    else await Task.Yield();
+    Console.WriteLine(answer);
 }
 
+#region nuget:System.Linq.Async
 
-#region Conventional try await Task catch   
-try
-{
-    var taskResult = await RequestAnswerOnMeaningOfLifeAsync();
-}
-catch
-{
-    // Handle error
-}
+// var asyncEnumerableObservable = Observable.Range(0, 100).ToAsyncEnumerable();
+// await foreach (var answer in asyncEnumerableObservable)
+// {
+//     Console.WriteLine(answer);
+// }
+// 
+// https://github.com/dotnet/reactive/blob/main/Ix.NET/Source/System.Linq.Async/System/Linq/Operators/ToAsyncEnumerable.Observable.cs
+
+// var observableAsyncEnumerable = AsyncEnumerable.Range(1,100)
+//      .ToObservable();
+//
+// https://github.com/dotnet/reactive/blob/main/Ix.NET/Source/System.Linq.Async/System/Linq/Operators/Range.cs
+// https://github.com/dotnet/reactive/blob/main/Ix.NET/Source/System.Linq.Async/System/Linq/Operators/ToObservable.cs
+
 # endregion
-
-# region Observable Exception Handling
-
-// In subcribers
-RequestAnswerOnMeaningOfLifeObservable()
-    .Subscribe(
-        observableResult =>
-            Console.WriteLine($"Observable Result: {observableResult}"),
-        exception =>
-            Console.WriteLine($"Observable.OnError handling - Woopsie!"));
-            
-
-
-
-// Catch via Observables
-RequestAnswerOnMeaningOfLifeObservable()
-    .Catch<int, Exception>(exception => Observable.Empty<int>()) // Catches all Exceptions
-    .Subscribe(
-        observableResult =>
-            Console.WriteLine(shouldThrow ? "I will never be called!" : $"Observable (with catch) Result: {observableResult}"),
-        exception =>
-            Console.WriteLine($"I will never be called!"));
-
-
-
-
-// Retry 
-var retries = 0;
-RequestAnswerOnMeaningOfLifeObservable()
-    .Do(_ => {}, ex => Console.WriteLine($"Observable (with retry) has failed '{++retries}' times!"))
-    .Retry(3) // or RetryWhen<T, Any>(Func<IObservable<Exception>, IObservable<Any>>): IObservable<T>
-    .Subscribe(
-        observableResult =>
-            Console.WriteLine($"Observable (with retry) Result: {observableResult}"),
-        exception =>
-            Console.WriteLine($"Observable.OnError (with retry) handling - Woopsie!"));
-
-// There are probably more
-
-#endregion
-
-#region API's
-// API's
-IObservable<int> RequestAnswerOnMeaningOfLifeObservable() 
-{
-    return SomeDangerousOperation()
-        .ToObservable()
-        .Select(_ => 42);
-}
-
-async Task<int> RequestAnswerOnMeaningOfLifeAsync() 
-{
-    await SomeDangerousOperation();
-
-    return 42;
-}
-
-#endregion
